4a735f323337577ea969db6eba8c9159fba709cb
Kail <bm>
2019-08-01 16:53:07 +1000

    things and stuff

---
+++ Assets/KailsAdditions/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+//make player be able to control
+//have thing for when player hits other player
+//make it networked
+//add shaders
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
